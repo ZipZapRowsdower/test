cbc9e0f63c1586245232b87daef12b2940430c51
Zap Rowsdower <zipzaprowsdower@gmail.com>
2020-10-18 09:11:35 -0400

    Add retro_api_version

--- hello-libretro/src/libretro.cpp
+++ hello-libretro/src/libretro.cpp
@@ -2,3 +2,5 @@
 
 RETRO_API void retro_init(void) {}
 RETRO_API void retro_deinit(void) {}
+
+RETRO_API unsigned retro_api_version(void) { return RETRO_API_VERSION; }
--- hello-libretro/tests/hello-libretro-test.cpp
+++ hello-libretro/tests/hello-libretro-test.cpp
@@ -10,6 +10,12 @@ TEST (RetroDeinit, Exists) {
     retro_deinit();
 }
 
+TEST (RetroApiVersion, ReturnsPublicAPIVersion) {
+    unsigned version;
+    version = retro_api_version();
+    EXPECT_EQ (version, RETRO_API_VERSION);
+}
+
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
