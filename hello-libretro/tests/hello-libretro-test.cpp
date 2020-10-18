4e2f4c1746a88abc102f22e1d147d724203afe2f
Zap Rowsdower <zipzaprowsdower@gmail.com>
2020-10-18 08:03:42 -0400

    Get testing working.

--- hello-libretro/tests/hello-libretro-test.cpp
+++ hello-libretro/tests/hello-libretro-test.cpp
@@ -1,3 +1,15 @@
-int main() {
-    return 0;
+#include "gtest/gtest.h"
+
+TEST (MyFirstTests, WillPass) {
+    EXPECT_EQ (true, true);
 }
+
+TEST (MyFirstTests, WillFail) {
+    EXPECT_EQ (true, false);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
